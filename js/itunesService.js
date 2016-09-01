cb41c72cc2b2503ea4bd405536862fc0de61bf82
ElishaGiles <elisha.g.giles@gmail.com>
2016-09-01 15:51:29 -0600

    completed project

--- js/itunesService.js
+++ js/itunesService.js
@@ -4,9 +4,23 @@ angular.module('itunes').service('itunesService', function($http, $q){
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
+  //Note that in the above line, artist is the parameter being passed in.
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-    
+
+    this.getMusic = function(artist, type) {
+      var deferred = $q.defer();
+      $http({
+        method: 'JSONP',
+        url: "https://itunes.apple.com/search?term=" + artist + "&callback=JSON_CALLBACK&type=" + type
+      })
+      .then(function(response){
+        var response = response.data.results;
+        deferred.resolve(response);
+        console.log(response);
+      })
+      return deferred.promise;
+    }
+
 });
